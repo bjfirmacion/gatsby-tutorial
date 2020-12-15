98b251d28160393fb10c05158366ce5906690e8b
BJ Firmacion <bj.firmacion@gmail.com>
2020-12-15 21:54:18 +0800

    removed person variable from Header.js example component

--- src/examples/Header.js
+++ src/examples/Header.js
@@ -14,14 +14,13 @@ const getData = graphql`
 `
 
 function Header() {
-  const { site: { info: { title, person: { name } } } } = useStaticQuery(getData);
+  const { site: { info: { title} } } = useStaticQuery(getData);
 
   return (
     <div>
       {/* <h1>title: {data.site.info.title}</h1> */}
       {/* <h1>name: {data.site.info.person.name}</h1> */}
       <h1>title: {title}</h1>
-      <h1>name: {name}</h1>
     </div>
   )
 }
